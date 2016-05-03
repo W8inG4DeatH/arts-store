c52425fcb700ebfca54faab6e32e044fd408a125
W8inG4DeatH <lipinski.m@o2.pl>
2016-05-03 19:49:25 +0200

    Landing page

--- src/js/app/controllers.js
+++ src/js/app/controllers.js
@@ -4,40 +4,42 @@
 
     app.controller('myController', ['$scope', '$http', '$window', 'mainService', function($scope, $http, $window, mainService){
 
-        $scope.slideTime = 500;
-        $scope.categories = ['animals','cars','movie','nature'];
+        $scope.galleryData = mainService.getGalleryData();
+        $scope.colorsForSlider = ['#1e3557','#37342d','#83293b'];
+        $scope.sliderCategoriesColors = [];
+        $scope.sliderCategoriesImageNumbers = [];
+        $scope.randomSlider = function() {
+            for (var i=0; i<$scope.galleryData.length; i++) {
+                $scope.sliderCategoriesColors[i] = $scope.colorsForSlider[mainService.GetRandomInt(0,$scope.colorsForSlider.length-1)];
+                $scope.sliderCategoriesImageNumbers[i] = mainService.GetRandomInt(1,$scope.galleryData[i].arts.length);
+            }
+        };
+        $scope.randomSlider();
 
         angular.element(document).ready(function() {
             mainService.StartTooltip();
-            //$('#menu-center-bar').outerWidth($('#slider').outerWidth());
+            mainService.StartSlider("w8-slider1",$scope.galleryData.length,4000,1000);
             // START
-            $('#site-loader').hide();
-            // logo
-            mainService.AnimateWord($('#logo > h1'),"A R T S",0,75);
-            mainService.AnimateWord($('#logo > p'),"S T O R E",600,33);
-            // menu-center // 1000
-            var MenuCenterShow = setTimeout(function() {
-                $('#menu-center').animate({opacity: '1'}, $scope.slideTime);
-            }, 1000);
-            // menu-right // 1500
-            var MenuRightShow = setTimeout(function() {
-                $('#menu-right').animate({opacity: '1'}, $scope.slideTime);
-            }, 1000);
-            // 2000
-        });
+            $scope.showWebsiteData = {
+                a1 : {mode: "AnimateWord", selector: "#logo > h1", word: "A R T S", stepTime: 75, delayTime: 0},
+                a2 : {mode: "AnimateWord", selector: "#logo > p", word: "S T O R E", stepTime: 33, delayTime: 0},
+                a3 : {mode: "FadeIn", selector: "#menu-center", stepTime: 500, delayTime: 0},
+                a4 : {mode: "FadeIn", selector: "#menu-right", stepTime: 500, delayTime: 0},
+            };
+            //mainService.ShowWebsite($scope.showWebsiteData);
+        }); 
 
         $scope.SlideUpAndDownByClass = function(elementClass) {
             $(elementClass).slideUp($scope.slideTime).slideDown($scope.slideTime);
         };
-
         $scope.SlideUpAndDownChildByClass = function(e,elementChildClass) {
             $(e.currentTarget).find(elementChildClass).slideUp($scope.slideTime).slideDown($scope.slideTime);
         };
    
-        $('.menu-center-link').mouseenter(function() {
+        $('.underline-link').mouseenter(function() {
             $(this).find('underline').animate({width: '100%'}, $scope.slideTime);
         });
-        $('.menu-center-link').mouseleave(function() {
+        $('.underline-link').mouseleave(function() {
             $(this).find('underline').animate({width: '0%'}, $scope.slideTime);
         });
 
